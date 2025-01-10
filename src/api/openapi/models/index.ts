fa359394083de8565d2bc249543e4d23c9463ce2
inhaltone <inhalt.xyz@gmail.com>
2025-01-10 17:45:21 +0200

    add: change path script and name of bin script to gen-api

---
+++ src/api/openapi/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PageUniEngineer';
+export * from './PageUniUserDTO';
+export * from './PageableObject';
+export * from './SortObject';
+export * from './UniEngineer';
+export * from './UniEngineerDTO';
+export * from './UniUserDTO';
