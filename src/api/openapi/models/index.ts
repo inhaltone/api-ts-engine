20058cfe217938b641aa608e690e944795cab230
inhaltone <inhalt.xyz@gmail.com>
2025-01-13 11:41:41 +0200

    remove api folder

--- src/api/openapi/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './PageUniEngineer';
-export * from './PageUniUserDTO';
-export * from './PageableObject';
-export * from './SortObject';
-export * from './UniEngineer';
-export * from './UniEngineerDTO';
-export * from './UniUserDTO';
